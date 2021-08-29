c04dc7527537599cc3e1a2823822c91315544696
PVSC <veerasaicharan@gmail.com>
2021-08-29 10:39:44 +0530

    1 file added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import './style.css';
+import PvComponent from './Second.js'
+export default function App() {
+  return (
+    <div>
+      <h1>TIMER PGM MOWA</h1>
+    
+<PvComponent/>
+    </div>
+  );
+}
+export default App;
