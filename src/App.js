2ecf311347ae673e02c8557dd7f4a542d3b946d4
PVSC <veerasaicharan@gmail.com>
2021-08-29 10:17:36 +0530

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./style.css";
 import PvComponent from './Second.js'
+
 export default function App() {
   return (
     <div>
+      <img src="./public/imagesFolder/Capture.PNG" alt="PV MOWA"></img>
     <PvComponent/>
     </div>
   );
